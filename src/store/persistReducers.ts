136e8d14e8ebaa783c8601c994136daaa6cce89e
lucas95santos <lucasa95santos@gmail.com>
2020-09-12 18:22:57 -0400

    Trocando a lib do AsyncStorage

--- src/store/persistReducers.ts
+++ src/store/persistReducers.ts
@@ -1,5 +1,4 @@
-// import storage from 'redux-persist/lib/storage';
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import { persistReducer } from 'redux-persist';
 
 // TODO: criar interface para reducers
